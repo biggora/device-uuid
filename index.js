e74b5007343b5625ef452ae6256180fc4f881e0e
Alexey Gordeyev <aleksej@gordejev.lv>
2017-01-07 23:29:30 +0200

    add NPM package please #1

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @revision    $Id: index.js 2012-03-24 16:21:10 Aleksey $
+ * @created     2016-09-24 16:21:10
+ * @category    Express Helpers
+ * @package     device-uuid
+ * @version     1.0.2
+ * @copyright   Copyright (c) 2016-2017 - All rights reserved.
+ * @license     MIT License
+ * @author      Alexey Gordeyev IK <aleksej@gordejev.lv>
+ * @link        http://www.gordejev.lv
+ *
+ */
+module.exports = require('./lib/device-uuid');
